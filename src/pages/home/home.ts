aef0a34aacd3df3bc083883b98e28c81ce8a29cd
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-08-01 00:26:27 -0300

    comecao a corrigir o problema com firebase

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,13 +3,16 @@ import { NavController, NavParams, LoadingController, IonicPage } from 'ionic-an
 import { AngularFireDatabase, FirebaseListObservable } from "angularfire2/database";
 import { AngularFireAuth } from "angularfire2/auth";
 
+import * as firebase from 'firebase';
+
 @IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  contas: FirebaseListObservable<any>;
+  contasFirebase: FirebaseListObservable<any>;
+  contas = [];
   usuario: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
@@ -25,10 +28,43 @@ export class HomePage {
     let loading = this.loadingCtrl.create();
     loading.present();
 
-    this.contas = this.afDatabase.list('contas');
-    this.contas.subscribe((contas) => {
-      loading.dismiss();
-    })
+    this.afDatabase.list('/contas')
+      .subscribe(contas => {
+        contas.forEach(conta => {
+          conta.prods = [];
+          Object.keys(conta.produtos).map(function (objectKey, index) {
+            var value = conta.produtos[objectKey];
+            conta.prods.push(conta.produtos[objectKey])
+          });
+        })
+        this.contas = contas;
+      });
+
+    loading.dismiss();
+  }
+
+  finalizar(conta, pedido) {
+    console.log("Finalizar", pedido)
+    console.log("teste", this.contas.indexOf(conta))
+    console.log("prod", this.contas[this.contas.indexOf(conta)].prods.indexOf(pedido))
+    console.log("fsfs", this.contas[this.contas.indexOf(conta)].prods[this.contas[this.contas.indexOf(conta)].prods.indexOf(pedido)]);
+
+    this.contas[this.contas.indexOf(conta)].prods[this.contas[this.contas.indexOf(conta)].prods.indexOf(pedido)].entregue = true;
+
+  }
+
+  save() {
+    this.afDatabase.list('/contas')
+      .subscribe(contas => {
+        contas.forEach(conta => {
+          conta.prods = [];
+          Object.keys(conta.produtos).map(function (objectKey, index) {
+            var value = conta.produtos[objectKey];
+            conta.prods.push(conta.produtos[objectKey])
+          });
+        })
+        this.contas = contas;
+      });
   }
 
 
